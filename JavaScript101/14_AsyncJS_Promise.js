84cb94137c28149654acee003c32fcca1482d0df
dsjramesh7 <rameshdussa07@gmail.com>
2024-07-07 23:17:30 +0530

    practice

--- JavaScript101/14_AsyncJS_Promise.js
+++ JavaScript101/14_AsyncJS_Promise.js
@@ -1,10 +1,24 @@
 //promise is object in js
 // three state pending, resolve/fullfilled , reject
-let promise = new Promise((resolve, reject) => {
-  console.log("I am a promise object");
-  resolve("success");
-});
- 
-promise.then((res) => {
-  console.log("promise fulfilled", res);
+
+// let promise = new Promise((resolve, reject) => {
+//   console.log("I am a promise object");
+//   resolve("success");
+// });
+
+// promise.then((res) => {
+//   console.log("promise fulfilled", res);
+// });
+
+
+
+function myOwnSetTimeout(duration) {
+  let p = new Promise(function (resolve) {
+    setTimeout(resolve, duration);
+  });
+  return p;
+}
+
+myOwnSetTimeout(3000).then(function () {
+  console.log("log the first thing");
 });
