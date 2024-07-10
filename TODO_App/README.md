3514c5ad3c9ac3ff7b31a0f65c448a209df393e0
dsjramesh7 <rameshdussa07@gmail.com>
2024-07-10 23:20:24 +0530

    practice

---
+++ TODO_App/README.md
@@ -0,0 +1,13 @@
+## Todo App
+
+This project contains  a simple TODO Application
+It has the following features:
+
+- Anyone can add task 
+- Anyone can delete task
+- Anyone can update the task
+- Anyone can checkmark the task if its completed
+- Anyone can see the existing task also
+
+// Initialize a node project
+// put a package.json
---
+++ TODO_App/backend/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos"
+);
+
+const todoSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const todo = mongoose.model("todos", todoSchema);
+
+module.exports = {
+  todo: todo,
+};
