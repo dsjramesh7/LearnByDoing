a55508a098283f34f995261b19b768cc0933fd72
dsjramesh7 <rameshdussa07@gmail.com>
2024-07-12 14:57:18 +0530

    practice

--- react-lessons/src/App.jsx
+++ react-lessons/src/App.jsx
@@ -2,27 +2,35 @@ import React from "react";
 import { useState } from "react";
 
 const App = () => {
-  const [title, setTitle] = useState("[click button to know]");
+  return (
+    <>
+      <HeadersWithButton />
+      <Headers title={"username is luffymugiwara"} />
+      <Headers title={"username is oioioi"} />
+      <Headers title={"username is rey"} />
+      <Headers title={"username is luffymugiwara"} />
+    </>
+  );
+};
 
-  const handleClick = () => {
+function HeadersWithButton() {
+  const [title, setTitle] = useState("click button to know");
+
+  const updateRollNumber = () => {
     const randomNumber = Math.ceil(Math.random() * 100);
     console.log(randomNumber);
     setTitle(randomNumber);
   };
   return (
-    <>
-      <Headers title={"my roll no is " + title} />
-      <Headers title={"username is luffymugiwara"} />
-
-      <br />
-
-      <button onClick={handleClick}>Click me</button>
-    </>
+    <div>
+      <h1>{`My Roll No is ${title}`}</h1>
+      <button onClick={updateRollNumber}>Click me</button>
+    </div>
   );
-};
+}
 
 function Headers({ title }) {
-  return <div>{title}</div>;
+  return <h2>{title}</h2>;
 }
 
 export default App;
